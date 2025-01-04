3579d97893d38530811397224c59897cc18e1e62
RodrigoDosSantos <rodrigosantospx@gmail.com>
2025-01-04 11:31:21 -0300

    first commit

---
+++ src/main/java/com/aceleradora/agilStore/AgilStoreApplication.java
@@ -0,0 +1,13 @@
+package com.aceleradora.agilStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleradora/agilStore/AgilStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleradora.agilStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
